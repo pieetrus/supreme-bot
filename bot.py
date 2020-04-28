755d5a803b77fb4105f386195d6d215f0913fd66
radosz99 <47248979+radosz99@users.noreply.github.com>
2020-04-28 18:28:22 +0200

    Change concept of searching

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import urllib.request as urllib2
 import urllib.error as url_error
 import time
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
 
 
 
@@ -31,8 +32,11 @@ def order(driver, k):
     #get product
     driver.get(k['product_url'])
     #add to basket
-    time.sleep(0.5)
-    driver.find_element_by_xpath('//*[@id="add-remove-buttons"]/input').click()
+    time.sleep(0.3)
+    try:
+        driver.find_element_by_xpath('//*[@id="add-remove-buttons"]/input').click()
+    except NoSuchElementException:
+        return False
     time.sleep(0.1)
     #go to basket
     driver.find_element_by_xpath('//*[@id="cart"]/a[2]').click()
@@ -65,7 +69,7 @@ def order(driver, k):
     #form-state
     driver.find_element_by_xpath('//*[@id="cart-cc"]/fieldset/p/label').click()
 
-
+    time.sleep(20)
     #process payment
     driver.find_element_by_xpath('//*[@id="pay"]/input').click()
 
@@ -84,35 +88,44 @@ def get_source_code(url):
 
     return text
 
-def get_item_url(key, color, category):
+def get_item_urls(key, category):
     code = get_source_code("https://www.supremenewyork.com/shop/all/" + category)
+    colour_with_links=[]
     while(True):
         position = code.find(key)
-        code = code[position:len(code)-1]
-        index = code[0:200].find(color)
-        if(index!=-1):
-            key_link = "href=\""
-            position = code.find(key_link)
-            code = code[position + len(key_link):position+200]
+        if(position==-1):
             break
-        else:
-            #uciecie na poczatku zeby nie znajdowalo znowu tej samej kolorystyki co wczesniej
-            code = code[len(key):len(code)-1]
+        code=code[position:len(code)]
+        key_link = "href=\""
+        position = code.find(key_link)
+        code = code[position + len(key_link):len(code)]
+        code_parts = code.split("\">")
+        link = code_parts[0]
+        code_parts=code_parts[1].split("<")
+        colour = code_parts[0]
+        colour_with_links.append((colour,"https://www.supremenewyork.com"+ link ))
+    
+    return colour_with_links
 
-
-    code_parts = code.split("\"")
-    print(code_parts[0])
-    return "https://www.supremenewyork.com/" + code_parts[0]
-
-
+def whether_sold_out(code):
+    if(code.find("sold_out")!=-1):
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
-    keys["product_url"]= get_item_url(key = "Digital", color = "Black", category="sweatshirts")
+    colour_with_links= get_item_urls(key = "Bloody", category="t-shirts")
+    print(colour_with_links)
     options = webdriver.ChromeOptions()
     options.add_argument('user-data-dir=www.supremenewyork.com')
     #options.add_argument('--headless')
     #options.add_experimental_option("detach", True)
     driver = webdriver.Chrome(options=options)
-    order(driver,keys)
+    for item in colour_with_links:
+        print("Przeglądam kolor " + item[0] + " pod linkiem " + item[1])
+        keys["product_url"]= item[1]
+        if(order(driver,keys)==False):
+            print("chuj sold out")
+            
     
 
